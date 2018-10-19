3e25ee6baba9b8aa09b9eec34012e7b77325057f
pngwn <hello@pngwn.io>
2018-10-19 12:09:54 +0100

    Remove redundant tests due to typescript.

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,21 +1,7 @@
 import { mockImports } from "./main";
 
-test("should return an object", () => {
-  expect(typeof mockImports()).toBe("object");
-});
-
-test("should have a name", () => {
-  expect(mockImports().name).toBeTruthy();
-});
-
-test("the name should be 'mock-imports'", () => {
-  expect(mockImports().name).toEqual("mock-imports");
-});
-
-test("should have a 'resolveId' method", () => {
-  expect(mockImports().resolveId).toBeTruthy();
-});
-
-test("'resolveId' should return a string", () => {
-  expect(typeof mockImports().resolveId("test", "test")).toBe("string");
+test("if there is no mock it should return null", () => {
+  expect(mockImports().resolveId("./index.js", "")).toBe(
+    "./__mocks__/index.js",
+  );
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import * as path from "path";
+
 interface ImportOptions {
   name: string;
   resolveId(importee: string, importer: string): string;
 }
 
-function resolveId(importee: string, importer: string): string {
-  return "hi";
+function resolveId(importee: string, importer: string | undefined): string {
+  if (importer === undefined) return null;
+  const absPath: string = path.resolve(
+    path.dirname(
+      "/Users/evilpingwin/Projects/svelte-test/node_modules/svelte/shared.js",
+    ),
+    "../../node_modules",
+  );
+  console.log(absPath);
+
+  return importee;
 }
 
 export function mockImports(): ImportOptions {
