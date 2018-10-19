f79696c142d6011b9a5b07d0865acbfa95af19f9
pngwn <hello@pngwn.io>
2018-10-19 15:40:37 +0100

    Add more type definitions.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,49 @@
 import * as path from "path";
 
-interface ImportOptions {
+interface UserOptions {
+  /**
+   * For each import should mocks be checked for and applied?\
+   * default: `true`
+   */
+  mockall?: boolean;
+  /**
+   * A pattern (string or RegExp) or array of patterns to be ignored for mocking purposes. This should match the imported module.\
+   * Only applies when `mockall: true`.\
+   * Default: `undefined`
+   */
+  ignore?: (RegExp | string)[] | RegExp | string;
+  /**
+   * A pattern (string or RegExp) or array of patterns to be included when mocking. This should match the imported module\
+   * Only applies when `mockall: false`.\
+   * Default: `undefined
+   */
+  mock?: (string | RegExp)[] | RegExp | string;
+}
+
+interface MockOptions {
   name: string;
-  resolveId(importee: string, importer: string): string;
+  resolveId(importee: string, importer: string): string | null;
 }
 
-function resolveId(importee: string, importer: string | undefined): string {
-  if (importer === undefined) return null;
-  const absPath: string = path.resolve(
-    path.dirname(
-      "/Users/evilpingwin/Projects/svelte-test/node_modules/svelte/shared.js",
-    ),
-    "../../node_modules",
-  );
-  console.log(absPath);
-
-  return importee;
-}
-
-export function mockImports(): ImportOptions {
+export function mockImports({
+  mockall = true,
+  ignore = "",
+  mock = "",
+}: UserOptions): MockOptions {
   return {
     name: "mock-imports",
-    resolveId,
+    resolveId(importee, importer): string {
+      if (!mockall || !importer) return null;
+
+      const absPath: string = path.resolve(
+        path.dirname(
+          "/Users/evilpingwin/Projects/svelte-test/node_modules/svelte/shared.js",
+        ),
+        "../../node_modules",
+      );
+      console.log(absPath);
+
+      // return importee;
+    },
   };
 }
