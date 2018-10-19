d46fa3f411fd73338c9050814a0d7d0d84badad5
pngwn <hello@pngwn.io>
2018-10-19 12:10:09 +0100

    Remove redundant tests due to typescript.

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,7 +1,7 @@
 import { mockImports } from "./main";
 
-test("if there is no mock it should return null", () => {
-  expect(mockImports().resolveId("./index.js", "")).toBe(
-    "./__mocks__/index.js",
-  );
-});
+// test("if there is no mock it should return null", () => {
+//   expect(mockImports().resolveId("./index.js", "")).toBe(
+//     "./__mocks__/index.js",
+//   );
+// });
