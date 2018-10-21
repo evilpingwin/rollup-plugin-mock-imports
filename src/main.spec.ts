36ef64821a2fa13caf1fc8328009dba74958d40e
pngwn <hello@pngwn.io>
2018-10-21 17:58:56 +0100

    I am very stupid. Fix path resolution in tests.

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,6 +1,7 @@
 import { mockImports } from "./main";
 
 // rudimentary tests to check file paths are being resolved as I _think_ they should be
+const path = process.cwd();
 
 test("if `mockall` is false and no mock pattern is passed `resolveId` shold return null", async () => {
   expect(
@@ -13,32 +14,26 @@ test("if `mockall` is false and no mock pattern is passed `resolveId` shold retu
 
 test("it should resolve node dependencies correctly", async () => {
   expect(await mockImports().resolveId("axios", "/some/path/to/file.js")).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/axios.js",
+    `${path}/node_mockdules/axios.js`,
   );
 });
 
 test("it should resolve node module mocks with slashes correctly", async () => {
   expect(
     await mockImports().resolveId("axios/index", "/some/path/to/file.js"),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/axios/index.js",
-  );
+  ).toBe(`${path}/node_mockdules/axios/index.js`);
 });
 
 test("it should resolve node module mocks with slashes and extensions correctly", async () => {
   expect(
     await mockImports().resolveId("axios/index.js", "/some/path/to/file.js"),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/axios/index.js",
-  );
+  ).toBe(`${path}/node_mockdules/axios/index.js`);
 });
 
 test("it should resolve node module mocks with multiple slashes correctly", async () => {
   expect(
     await mockImports().resolveId("lodash/fp/curryN", "/some/path/to/file.js"),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/lodash/fp/curryN.js",
-  );
+  ).toBe(`${path}/node_mockdules/lodash/fp/curryN.js`);
 });
 
 test("it should resolve node module mocks with relative paths correctly", async () => {
@@ -48,81 +43,56 @@ test("it should resolve node module mocks with relative paths correctly", async 
   expect(
     await mockImports().resolveId(
       "../node_modules/axios/index.js",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/axios/index.js",
-  );
+  ).toBe(`${path}/node_mockdules/axios/index.js`);
 });
 
 test("it should resolve node module mocks with relative paths and no extensions correctly", async () => {
   expect(
     await mockImports().resolveId(
       "../node_modules/axios/index",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/axios/index.js",
-  );
+  ).toBe(`${path}/node_mockdules/axios/index.js`);
 });
 
 test("it should resolve node builtin mocks correctly: 'fs'", async () => {
-  expect(
-    await mockImports().resolveId(
-      "fs",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/fs.js",
+  expect(await mockImports().resolveId("fs", `${path}/src/index.js`)).toBe(
+    `${path}/node_mockdules/fs.js`,
   );
 });
 
 test("if no builtin mock is present it should return null", async () => {
-  expect(
-    await mockImports().resolveId(
-      "path",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
-  ).toBe(null);
+  expect(await mockImports().resolveId("path", `${path}/src/index.js`)).toBe(
+    null,
+  );
 });
 
 test("it should resolve local module mocks correctly", async () => {
   expect(
     await mockImports().resolveId(
       "../tests/something.js",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/tests/__mocks__/something.js",
-  );
+  ).toBe(`${path}/tests/__mocks__/something.js`);
 });
 
 test("it should resolve local module mocks with no extension correctly", async () => {
   expect(
-    await mockImports().resolveId(
-      "../tests/something",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/tests/__mocks__/something.js",
-  );
+    await mockImports().resolveId("../tests/something", `${path}/src/index.js`),
+  ).toBe(`${path}/tests/__mocks__/something.js`);
 });
 
 test("if there is no matching mock function, it should return null", async () => {
   expect(
-    await mockImports().resolveId(
-      "fs-extra",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
+    await mockImports().resolveId("fs-extra", `${path}/src/index.js`),
   ).toBe(null);
 });
 
 test("if a module does not exist it should return null", async () => {
   expect(
-    await mockImports().resolveId(
-      "whatever",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
+    await mockImports().resolveId("whatever", `${path}/src/index.js`),
   ).toBe(null);
 });
 
@@ -132,10 +102,7 @@ test("if there is no importer `resolveId` should return null", async () => {
 
 test("ignore options should be respected: string", async () => {
   expect(
-    await mockImports({ ignore: "fs" }).resolveId(
-      "fs",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
+    await mockImports({ ignore: "fs" }).resolveId("fs", `${path}/src/index.js`),
   ).toBe(null);
 });
 
@@ -143,7 +110,7 @@ test("ignore options should be respected: string[]", async () => {
   expect(
     await mockImports({ ignore: ["fs", "axios"] }).resolveId(
       "fs",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
   ).toBe(null);
 });
@@ -152,7 +119,7 @@ test("ignore options should be respected: RegExp", async () => {
   expect(
     await mockImports({ ignore: /ios/ }).resolveId(
       "axios",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
   ).toBe(null);
 });
@@ -161,7 +128,7 @@ test("ignore options should be respected: RegExp[]", async () => {
   expect(
     await mockImports({ ignore: [/^a/, /fs/] }).resolveId(
       "axios",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
   ).toBe(null);
 });
@@ -169,29 +136,19 @@ test("ignore options should be respected: RegExp[]", async () => {
 test("built in modules should be mocked even if the node cwd is not root", async () => {
   // this is probably not smart
   const temp = process.cwd;
-  const path = process.cwd();
   process.cwd = () => `${path}/some/random/path/that/is/long`;
-  expect(
-    await mockImports().resolveId(
-      "fs",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/fs.js",
+  expect(await mockImports().resolveId("fs", `${path}/src/index.js`)).toBe(
+    `${path}/node_mockdules/fs.js`,
   );
   process.cwd = temp;
 });
 
 test("built in modules should return null if the root app path cannot be resolved", async () => {
   const temp = process.cwd;
-  const path = process.cwd();
   process.cwd = () => `/some/random/path/that/is/wrong`;
-  expect(
-    await mockImports().resolveId(
-      "fs",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-    ),
-  ).toBe(null);
+  expect(await mockImports().resolveId("fs", `${path}/src/index.js`)).toBe(
+    null,
+  );
   process.cwd = temp;
 });
 
@@ -199,10 +156,7 @@ test("built in modules should return log a warning if the root app path cannot b
   const temp = process.cwd;
   process.cwd = () => `/some/random/path/that/is/wrong`;
   const spy = spyOn(console, "warn");
-  await mockImports().resolveId(
-    "fs",
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
-  );
+  await mockImports().resolveId("fs", `${path}/src/index.js`);
   expect(spy).toBeCalledWith(
     "Couldn't resolve root path. Deferring to default module resolution.",
   );
@@ -224,9 +178,7 @@ test("with `mockall: false` `mock` patterns should be respected: string", async 
       "axios",
       "/some/path/to/file.js",
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/axios.js",
-  );
+  ).toBe(`${path}/node_mockdules/axios.js`);
 });
 
 test("with `mockall: false` `mock` patterns should be respected: string[]", async () => {
@@ -235,38 +187,32 @@ test("with `mockall: false` `mock` patterns should be respected: string[]", asyn
       "fs",
       "/some/path/to/file.js",
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/node_mockdules/fs.js",
-  );
+  ).toBe(`${path}/node_mockdules/fs.js`);
 });
 
 test("with `mockall: false` `mock` patterns should be respected: RegExp", async () => {
   expect(
     await mockImports({ mockall: false, mock: /\.js/ }).resolveId(
       "../tests/something.js",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/tests/__mocks__/something.js",
-  );
+  ).toBe(`${path}/tests/__mocks__/something.js`);
 });
 
 test("with `mockall: false` `mock` patterns should be respected: RegExp[]", async () => {
   expect(
     await mockImports({ mockall: false, mock: [/hail/, /some/] }).resolveId(
       "../tests/something",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
-  ).toBe(
-    "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/tests/__mocks__/something.js",
-  );
+  ).toBe(`${path}/tests/__mocks__/something.js`);
 });
 
 test("`mockall: false` with non-matching `mock` patterns should be ignored", async () => {
   expect(
     await mockImports({ mockall: false, mock: /xyz/ }).resolveId(
       "../tests/something",
-      "/Users/evilpingwin/Projects/rollup-plugin-mock-imports/src/index.js",
+      `${path}/src/index.js`,
     ),
   ).toBe(null);
 });
